09597f88ee36b16b9f5afd84ad080fb2a804b784
rahul <rahulraoji9024@gmail.com>
2024-08-30 18:56:55 +0530

    first commit

---
+++ calcularor/calculator.js
@@ -0,0 +1,29 @@
+(function() {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            if (value !== undefined) {
+                screen.value += value;
+            }
+        });
+    });
+    equal.addEventListener('click', function(e) {
+        if (screen.value === '') {
+            screen.value = "Please enter";
+        } else {
+            try {
+                let answer = eval(screen.value);
+                screen.value = answer;
+            } catch (error) {
+                screen.value = "Error";
+            }
+        }
+    });
+    clear.addEventListener('click', function(e) {
+        screen.value = "";
+    });
+})()
